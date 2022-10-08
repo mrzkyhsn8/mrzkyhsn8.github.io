95081e7d07403226016e820ef4e677aeed5d0c5a
mrzkyhsn8 <mrizkyhusain8@gmail.com>
2022-10-08 11:15:52 +0700

    fix bug color & improve anim

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,7 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .work__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .work__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .work__subtitle, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon, .work__img img, .work__text',{ interval: 200}); 
+sr.reveal('.skills__data, .contact__input, .contact__button',{interval: 200}); 
